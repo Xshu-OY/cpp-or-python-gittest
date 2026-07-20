9c059993c5b1c91b6edf90bc5ff913a04335ef40
oyxy <oyxy@qq.com>
2026-07-20 17:17:30 +0800

    数组test

--- array.py
+++ array.py
@@ -4,6 +4,12 @@ def random_access(nums:list[int])->int:
     random_index=random.randint(0,len(nums)-1)
     return nums[random_index]
 
+def insert(nums:list[int],num:int,index:int)->list[int]:
+    for i in range(len(nums)-1,index,-1):
+        nums[i]=nums[i-1]
+    nums[index]=num
+    return nums
+
 
 if __name__=="__main__":
     nums=[2,7,4,1,5,3,6]
